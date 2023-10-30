2bb49ed6dba6f9af8a756395c89048e74cd83d73
keyne <kleuyckx@gmail.com>
2023-10-30 16:30:39 +0000

    Add soulbound flag

--- src/assetnft.rs
+++ src/assetnft.rs
@@ -9,6 +9,7 @@ pub const BURNING: u32 = 0;
 pub const FREEZING: u32 = 1;
 pub const WHITELISTING: u32 = 2;
 pub const DISABLE_SENDING: u32 = 3;
+pub const SOULBOUND: u32 = 4;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
